fb50e02f342df3c335b3bd69c742df9ffb9a024b
Yashwant Bokadia <bokadiayashwant@gmail.com>
2017-03-25 06:53:14 +0530

    add timer and creat table dynamically

--- dashboard.php
+++ dashboard.php
@@ -82,3 +82,34 @@
 						</ul>
 					</div>
 				</div>
+				</nav>
+				<h2><span id="countdown" class="timer" style="padding-top: 10px;float: right;padding-right:10px;"></span></h2>
+<script>
+var upgradeTime = 9000;
+var seconds = upgradeTime;
+function timer() {
+    var days        = Math.floor(seconds/24/60/60);
+    var hoursLeft   = Math.floor((seconds) - (days*86400));
+    var hours       = Math.floor(hoursLeft/3600);
+    var minutesLeft = Math.floor((hoursLeft) - (hours*3600));
+    var minutes     = Math.floor(minutesLeft/60);
+    var remainingSeconds = seconds % 60;
+    if (remainingSeconds < 10) {
+        remainingSeconds = "0" + remainingSeconds; 
+    }
+     if (minutes < 10) {
+        minutes = "0" + minutes; 
+    }
+    if (hours < 10) {
+        hours = "0" + hours; 
+    }
+    document.getElementById('countdown').innerHTML =  hours + ":" + minutes + ":" + remainingSeconds;
+    if (seconds == 0) {
+        clearInterval(countdownTimer);
+        document.getElementById('countdown').innerHTML = "Completed";
+    } else {
+        seconds--;
+    }
+}
+var countdownTimer = setInterval('timer()', 1000);
+</script>
--- signup.php
+++ signup.php
@@ -9,30 +9,31 @@ $pass = $_POST['password'];
 $phone = $_POST['phone'];
 $passkey = $_POST['Passkey'];
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+	$conn = mysql_connect('localhost', 'root', '');
+	if (!$conn)
+	{
+		die("Database Connection Failed" . mysql_error());
+	}
+	$select_db = mysql_select_db('oep');
+	if (!$select_db)
+	{
+		die("Database Selection Failed" . mysql_error());
+	}
 
-$sql = "INSERT INTO `institute` VALUES ('".$institute."','".$mail."','".$pass."','".$phone."','".$passkey."')";
-
-$sql1 = "CREATE TABLE ".$institute." (
-id VARCHAR(30) AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(30) NOT NULL,
-rollno VARCHAR(15),
-email VARCHAR(50),
-password VARCHAR(20),
-branch TEXT(5)
-)";
-
-
-if ($conn->query($sql1) === TRUE) {
-	   echo "created sucessfully";
-} else {
-    echo "Error: " . $sql1 . "<br>" . $conn->error;
+$sql = "INSERT INTO `institute` VALUES ('".$institute."','".$mail."','".md5($pass)."','".$phone."','".$passkey."')";
+ mysql_select_db( 'oep' );
+$retval = mysql_query("CREATE TABLE ".$institute." (".
+".id VARCHAR(30) ,".
+".name VARCHAR(30) NOT NULL,".
+".rollno VARCHAR(15),".
+".email VARCHAR(50),".
+".password VARCHAR(20),".
+".branch TEXT(5)
+)",$conn);
+if(! $retval )
+{
+  die('Could not create table: ' . mysql_error());
 }
-
+header("location:login.php");
 $conn->close();
 ?>
